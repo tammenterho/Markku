05aa2d5661798986cc861495a01f096ffb4466d9
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-25 11:37:04 +0200

    npm audit, mantine init, app shell

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
 import "./App.css";
 import { CampaignList } from "./components/campaignList";
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 function App() {
+  const [opened, { toggle }] = useDisclosure();
   return (
-    <>
-      <CampaignList />
-    </>
+    <AppShell
+      padding="md"
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+
+        <div>Markku</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+      <AppShell.Main>
+        Main
+        <CampaignList />
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
