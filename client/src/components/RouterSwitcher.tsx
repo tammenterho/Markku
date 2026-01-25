553d0ce26d9596d2227ee861c1347552c31aef5a
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-25 15:33:43 +0200

    settings

--- client/src/components/RouterSwitcher.tsx
+++ client/src/components/RouterSwitcher.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./NotFound";
-import Home from "./Home";
 import CreateCampaign from "./CreateCampaign";
 import { CampaignList } from "./campaignList";
+import Settings from "./Settings";
 
 const RouterSwitcher = () => {
   return (
     <Routes>
       <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<CampaignList />} />
+      <Route path="/settings" element={<Settings />} />
       <Route path="/campaign" element={<CampaignList />} />
       <Route path="/new" element={<CreateCampaign />} />
     </Routes>
