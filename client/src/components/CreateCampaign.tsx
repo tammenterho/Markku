675397bea5931dc3402d5906da5da6b7aecea7e2
Mikko Terho <terho.mikko@gmail.com>
2026-01-25 15:36:11 +0200

    asfd

--- client/src/components/CreateCampaign.tsx
+++ client/src/components/CreateCampaign.tsx
@@ -1,16 +1,13 @@
-import { Radio, Select, TextInput, Title } from "@mantine/core";
+import { Button, Group, Radio, Select, TextInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useState } from "react";
-import { DatePicker, DatePickerInput } from "@mantine/dates";
+import { DatePickerInput } from "@mantine/dates";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const data = [
-  { label: "Yritys A" },
-  { label: "Yritys B" },
-  { label: "Yritys C" },
-];
+const companyData = ["Yritys A", "Yritys B", "Yritys C"];
 
 const CreateCampaign = () => {
-  const [checked, setChecked] = useState(false);
+  const navigate = useNavigate();
 
   const form = useForm({
     initialValues: {
@@ -18,9 +15,9 @@ const CreateCampaign = () => {
       name: "",
       payer: "",
       budget: "",
-      budgetPeriod: "",
-      startDate: "",
-      endDate: "",
+      budgetPeriod: "Duration",
+      startDate: null as Date | null,
+      endDate: null as Date | null,
       targetArea: "",
       targetDemographic: "",
       gender: "",
@@ -32,39 +29,85 @@ const CreateCampaign = () => {
     },
   });
 
+  const handleSubmit = async (values: typeof form.values) => {
+    const campaignData = {
+      clientId: "659e7d23473b8d69cb77c2fb",
+      companyId: "659e7d23473b8d69cb77c2fb",
+      type: "AD",
+      company: values.company,
+      name: values.name,
+      customer: values.payer,
+      budget: Number(values.budget) || 0,
+      start: values.startDate,
+      end: values.endDate,
+      targetArea: values.targetArea,
+      targetAge: values.targetDemographic,
+      title: values.adTitle,
+      copyText: values.adText,
+    };
+
+    try {
+      await axios.post("http://localhost:3000/campaigns", campaignData);
+      navigate("/campaign");
+    } catch (error) {
+      console.error("Error creating campaign:", error);
+    }
+  };
+
   return (
-    <form onSubmit={form.onSubmit(() => {})}>
+    <form onSubmit={form.onSubmit(handleSubmit)}>
       <Title>Luo Uusi</Title>
-      <Select component="select" label="Company" name="company">
-        {data.map((company) => (
-          <option key={company.label} value={company.label}>
-            {company.label}
-          </option>
-        ))}
-      </Select>
-      <TextInput label="Name" name="name" />
-      <TextInput label="Payer" name="payer" />
-      <TextInput label="Budget" name="budget" />
-      <p>Use budget</p>
-      <Radio
-        label="Day"
-        value="monthly"
-        name="budgetPeriod"
-        checked={checked}
-        onChange={(event) => setChecked(event.currentTarget.checked)}
+      <h2>General</h2>
+      <Select
+        label="Company"
+        placeholder="Valitse yritys"
+        data={companyData}
+        {...form.getInputProps("company")}
       />
-      <Radio
-        label="Whole duration"
-        value="monthly"
-        name="budgetPeriod"
-        checked={checked}
-        onChange={(event) => setChecked(event.currentTarget.checked)}
+      <TextInput label="Name" {...form.getInputProps("name")} />
+      <Group>
+        <TextInput label="Payer" {...form.getInputProps("payer")} />
+        <TextInput
+          label="Budget"
+          type="number"
+          {...form.getInputProps("budget")}
+        />
+        <Radio.Group
+          name="budgetPeriod"
+          label="Use budget"
+          {...form.getInputProps("budgetPeriod")}
+        >
+          <Group mt="xs">
+            <Radio value="Day" label="Day" />
+            <Radio value="Duration" label="Whole duration" />
+          </Group>
+        </Radio.Group>
+      </Group>
+      <Group>
+        <DatePickerInput
+          label="Start date"
+          {...form.getInputProps("startDate")}
+        />
+        <DatePickerInput label="End date" {...form.getInputProps("endDate")} />
+      </Group>
+      <h2>Target</h2>
+
+      <TextInput label="Target area" {...form.getInputProps("targetArea")} />
+      <TextInput
+        label="Target demographic"
+        {...form.getInputProps("targetDemographic")}
       />
-      <DatePickerInput label="Start date" name="startDate" />
-      <DatePickerInput label="End date" name="endDate" />
-      <TextInput label="Target area" name="targetArea" />
-      <TextInput label="Target demographic" name="targetDemographic" />
-      <TextInput label="Gender" name="gender" />
+      <TextInput label="Gender" {...form.getInputProps("gender")} />
+      <h2>Campaign</h2>
+
+      <TextInput label="Ad title" {...form.getInputProps("adTitle")} />
+      <TextInput label="Ad text" {...form.getInputProps("adText")} />
+      <TextInput label="Media info" {...form.getInputProps("mediaInfo")} />
+      <TextInput label="Ad URL" {...form.getInputProps("adUrl")} />
+      <TextInput label="CTA" {...form.getInputProps("CTA")} />
+      <Group justify="flex-end" mt="md">
+        <Button type="submit">Submit</Button>
+      </Group>
     </form>
   );
 };
