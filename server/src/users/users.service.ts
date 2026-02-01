dc5233c754336c2f4cca45023d9f2aaa5c6f20c0
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-02-01 15:36:23 +0200

    basic user service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findByUsername(username: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+  create(username: string, passwordHash: string): Promise<User> {
+    const user = this.usersRepository.create({ username, passwordHash });
+    return this.usersRepository.save(user);
+  }
+}
