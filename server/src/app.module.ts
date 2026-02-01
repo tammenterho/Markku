66cbdbff3dca1c7941d3cda93d14f3c65e75e3fb
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-02-01 12:54:04 +0200

    user and auth module and service init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CampaignsModule } from './campaigns.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CampaignsModule],
+  imports: [CampaignsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
