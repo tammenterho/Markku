7f96397740d0e24062e85e2394e37388630d4744
Mikko Terho <terho.mikko@gmail.com>
2026-02-14 16:25:45 +0200

    fix: tests

--- client/src/utils/test-utils.tsx
+++ client/src/utils/test-utils.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { type ReactElement } from "react";
 import { render, type RenderOptions } from "@testing-library/react";
 import { MantineProvider } from "@mantine/core";
