9a658e3e9347fa101cbabd60d4a90a3197e2e191
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-02-20 11:53:45 +0200

    BASE_DIR fix

--- .scripts/demo-deploy.sh
+++ .scripts/demo-deploy.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export APP_NAME="markku-api-demo"
-export BASE_DIR="/var/www/markku-demo"
+export BASE_DIR="/var/www/demo-markku"
 export DEPLOY_BRANCH="demo"
 export GIT_REMOTE="origin"
 
