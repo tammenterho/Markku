bbb8fd37199b139e45e228ff4eec4de45b65eea1
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-04-26 16:43:12 +0300

    Migrations fixes (#80)
    * migrations fix
    
    * more migrations fixes

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -294,7 +294,7 @@ if [[ ! -f "$SHARED_ENV_FILE" ]]; then
 	exit 1
 fi
 run_with_privilege ln -sfn "$SHARED_ENV_FILE" .env
-npm run migration:run
+npm run migration:run:prod
 
 NEW_SERVER_RELEASE="$RELEASES_DIR/server/$TIMESTAMP"
 run_with_privilege mkdir -p "$NEW_SERVER_RELEASE"
