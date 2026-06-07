76785d9be26dab1fe189d14833bc0dd7ebc26ac8
Mikko Terho <terho.mikko@gmail.com>
2026-06-07 13:59:07 +0300

    fix migration

--- server/migrations/1769951188270-InitTables.ts
+++ server/migrations/1769951188270-InitTables.ts
@@ -5,7 +5,7 @@ export class InitTables1769951188270 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "passwordHash" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+      `CREATE TABLE IF NOT EXISTS "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "passwordHash" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
     );
   }
 
--- server/migrations/1771924968369-InitTables.ts
+++ server/migrations/1771924968369-InitTables.ts
@@ -4,13 +4,26 @@ export class InitTables1771924968369 implements MigrationInterface {
   name = 'InitTables1771924968369';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "campaigns" DROP COLUMN "clientId"`);
     await queryRunner.query(
-      `ALTER TABLE "companies" ALTER COLUMN "linkId" DROP DEFAULT`,
+      `ALTER TABLE "campaigns" DROP COLUMN IF EXISTS "clientId"`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "companies" ALTER COLUMN "linkId" SET DEFAULT gen_random_uuid()`,
-    );
+
+    await queryRunner.query(`
+      DO $$
+      BEGIN
+        IF EXISTS (
+          SELECT 1
+          FROM information_schema.columns
+          WHERE table_schema = 'public'
+            AND table_name = 'companies'
+            AND column_name = 'linkId'
+        ) THEN
+          ALTER TABLE "companies" ALTER COLUMN "linkId" DROP DEFAULT;
+          ALTER TABLE "companies" ALTER COLUMN "linkId" SET DEFAULT gen_random_uuid();
+        END IF;
+      END
+      $$;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
