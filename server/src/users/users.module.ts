7bf0aa575d0d461677998105897d5d3e7140c12f
Leevi Ossi <leevi@leeviossi.com>
2026-06-07 18:14:48 +0300

    typescript errors

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,14 +1,13 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { CompaniesModule } from '../companies/companies.module';
+import { Company } from '../companies/companies.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
-    forwardRef(() => CompaniesModule),
+    TypeOrmModule.forFeature([User, Company]),
   ],
   controllers: [UsersController],
   providers: [UsersService],
